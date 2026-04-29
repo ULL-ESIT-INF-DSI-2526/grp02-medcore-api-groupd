0f334bdb428d6b64dc7978896dd62d017291336f
Nombre <91544254+DiegoPrietoG@users.noreply.github.com>
2026-04-29 13:56:18 +0100

    forma más modular de crear endpoint para hacerlo en archivos diferentes y así usar Router

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import './db/mongoose.js';
+import staffRouter from './routers/staff.js';
 
 export const app = express();
 app.use(express.json());
+app.use('/staff', staffRouter);
