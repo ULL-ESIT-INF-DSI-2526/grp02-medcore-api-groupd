bf73aa98d3541a3bec0227017a9999c5dff46cbc
Pablo Perez Hernandez <alu0101619002@ull.edu.es>
2026-05-09 09:02:22 +0000

    feat: add recordRouter for handling patient records routes

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import staffRouter from './routers/staff.js';
 import { patientRouter } from './routers/patients.js';
 import { medicationRouter } from './routers/medications.js';
 import cors from 'cors';
+import { recordRouter } from './routers/records.js';
 
 export const app = express();
 app.use(cors());
@@ -10,3 +11,4 @@ app.use(express.json());
 app.use('/staff', staffRouter);
 app.use(patientRouter);
 app.use('/medications', medicationRouter);
+app.use('/record', recordRouter);
